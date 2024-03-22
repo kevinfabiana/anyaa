af0995b43be7cc00a68853e942ce17c9e7b37140
kevinfabiana <123175604+kevinfabiana@users.noreply.github.com>
2024-03-22 14:09:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+var audio = document.getElementById("myAudio");
+
+// Fungsi untuk memulai atau memutar ulang lagu
+function playAudio() {
+  audio.play();
+}
+
+// Panggil fungsi playAudio() saat halaman dimuat
+window.onload = playAudio;
 
       
